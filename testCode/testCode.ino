5ff61d28b0702664fc54d48e4434575825cdad22
arcticwaffle <bradarnot@gmail.com>
2015-10-31 15:42:48 -0600

    Updated test code for testing claw mechanics

---
+++ testCode/testCode.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+Servo serv;
+
+void setup() {
+  // put your setup code here, to run once:
+  serv.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //serv.write(0);
+  sweep(0, 90, 20);
+}
+
+void sweep(int start, int en, int del) {
+  for(int i = start; i <= en; i++) {
+    serv.write(i);
+    delay(del);
+  }
+  for(int i = en; i >= start; i--) {
+    serv.write(i);
+    delay(del+15);
+  }
+}
+
