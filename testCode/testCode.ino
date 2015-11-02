75a901596d58e338bf929820b18712927ad1e678
arcticwaffle <bradarnot@gmail.com>
2015-11-01 18:37:30 -0700

    Added method for serial input in test code

--- testCode/testCode.ino
+++ testCode/testCode.ino
@@ -5,12 +5,27 @@ Servo serv;
 void setup() {
   // put your setup code here, to run once:
   serv.attach(9);
+  //Serial.begin(9600);
+  //while (! Serial); // Wait until Serial is ready
+  //Serial.println("Enter o or c to open or close the claw");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //serv.write(0);
-  sweep(0, 90, 20);
+  //sweep(0, 48, 20);
+  //serialInput(0, 48);
+}
+
+void serialInput(int op, int cl) {
+  if (Serial.available()) {
+    char c = Serial.read();
+    if (c == 'o') {
+      serv.write(op);
+    } else if (c == 'c') {
+      serv.write(cl);
+    }
+  }
 }
 
 void sweep(int start, int en, int del) {
@@ -20,7 +35,7 @@ void sweep(int start, int en, int del) {
   }
   for(int i = en; i >= start; i--) {
     serv.write(i);
-    delay(del+15);
+    delay(del);
   }
 }
 
