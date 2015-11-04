feba8dd664d348bf3683a6d7cc38316f8e4145a0
arcticwaffle <bradarnot@gmail.com>
2015-11-04 10:29:30 -0700

    Merge brances

---
+++ Test_Code3/Test_Code3.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+
+Servo serv;
+int sensorPin = A0;
+int pinput = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  serv.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogRead(sensorPin);
+  serv.write(pinput);
+}
+
+
