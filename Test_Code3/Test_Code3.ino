fd23464350d34306140449d4ff8c7838287dd988
kedo8480 <kedo8480@colorado.edu>
2015-11-10 10:55:10 -0700

    Updated Test_Code3

--- Test_Code3/Test_Code3.ino
+++ Test_Code3/Test_Code3.ino
@@ -1,18 +1,21 @@
 #include <Servo.h>
 
 Servo serv;
+//Servo serv1;
 int sensorPin = A0;
-int pinput = 0;
+int pinput = 45;
 
 void setup() {
   // put your setup code here, to run once:
   serv.attach(9);
+  //serv.attach(10);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogRead(sensorPin);
+  pinput = analogRead(sensorPin);
   serv.write(pinput);
+  //serv1.write(pinput);
 }
 
 
