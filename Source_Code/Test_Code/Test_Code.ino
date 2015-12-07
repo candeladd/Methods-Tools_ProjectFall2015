c14be5af31d36aa2d94b4e71bedf5c867371b8d1
kedo8480 <kedo8480@colorado.edu>
2015-12-07 13:34:56 -0700

    Organizing Repo

---
+++ Source_Code/Test_Code/Test_Code.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+Servo myservo;
+//Servo myservo2;
+
+void setup() {
+ myservo.attach(3);
+ //myservo2.attach(5);
+}
+
+void loop() {
+  myservo.write(180);
+  delay(100);
+  myservo.write(0);
+  delay(100);
+// for(int i=0; i<180; i++) {
+//  myservo.write(i);
+//  myservo2.write(i);
+//  delay(15);
+// } 
+//  for(int i=180; i>180; i--) {
+//  myservo.write(i);
+//  myservo2.write(i);
+//  delay(15);
+// } 
+}
