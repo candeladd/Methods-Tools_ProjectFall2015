310afdf7ab7e9f8bb2c60bae379ddf7a4401fce6
kedo8480 <kedo8480@colorado.edu>
2015-12-07 17:04:11 -0700

    Added auto-doc comments

--- Source_Code/Final_Demo_Code.ino
+++ Source_Code/Final_Demo_Code.ino
@@ -1,40 +1,33 @@
-/*************************************************** 
-  This is an example for our Adafruit 16-channel PWM & Servo driver
-  Servo test - this will drive 16 servos, one after the other
+/*! \brief This is the final demo code.
+ *         We used this in the presentation.
+ *
+ *  This code sweeps the base of the robot, then the upper arm, then the claw.
+ *  The code loops infinitely until power is cut from the arduino or the shield.
+ */
 
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/815
-
-  These displays use I2C to communicate, 2 pins are required to  
-  interface. For Arduino UNOs, thats SCL -> Analog 5, SDA -> Analog 4
-
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-// called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
 
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
-#define SERVOMINBASE  150 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAXBASE  300 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVOMINBASE  150 // this is the 'minimum' pulse length base and claw
+#define SERVOMAXBASE  300 // this is the 'maximum' pulse length base and claw
 
-#define SERVOMINUP  150 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAXUP  250 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVOMINUP  150 // this is the 'minimum' pulse length upper arm
+#define SERVOMAXUP  250 // this is the 'maximum' pulse length upper arm
+
 // our servo # counter
 uint8_t servonum = 0;
+
+/*! \brief This sets up the Arduino code.
+ *         Runs only once at the beginning.
+ *
+ *  Serial.begin(9600) sets up the serial monitor for debugging purposes.
+ *  The next line simply prints to the serial monitor.
+ *  The last chunk sets up the servo shield. It sets up the pwm and wires
+ *  in the shield.
+ */
 
 void setup() {
   Serial.begin(9600);
@@ -51,21 +44,16 @@ void setup() {
   yield();
 }
 
-// you can use this function if you'd like to set the pulse length in seconds
-// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. its not precise!
-void setServoPulse(uint8_t n, double pulse) {
-  double pulselength;
-  
-  pulselength = 1000000;   // 1,000,000 us per second
-  pulselength /= 60;   // 60 Hz
-  Serial.print(pulselength); Serial.println(" us per period"); 
-  pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength); Serial.println(" us per bit"); 
-  pulse *= 1000;
-  pulse /= pulselength;
-  Serial.println(pulse);
-  pwm.setPWM(n, 0, pulse);
-}
+/*! \brief This code loops through every cycle.
+ *         It keeps cycling until power is disconnected.
+ *
+ *  There are two sets of for loops for each joint. The first for loop
+ *  sweeps the servo up, the second for loop sweeps the servo down. 
+ *  There are delays in between each loop to ensure that the servo doesn't
+ *  move too fast and burn out. There are different servo min and maxes
+ *  based on the physical arcitecture of the arm. We ensured that each part
+ *  moved within range of the table, etc.
+ */
 
 void loop() {
   // Drive each servo one at a time
